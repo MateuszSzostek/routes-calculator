eb972cdf65797cf4e4773682dca85a20be97b684
MateuszSzostek <mszostek@oakfusion.pl>
2023-12-06 09:36:45 +0100

    MapView: adding new navigation points on click on map added

--- src/modules/map/MapView.tsx
+++ src/modules/map/MapView.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import L from "leaflet";
+import React, { useEffect, useState } from "react";
+import L, { LatLngExpression } from "leaflet";
 import { TileLayer, Popup, Marker, useMap } from "react-leaflet";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 import "leaflet-routing-machine";
@@ -8,22 +8,38 @@ import "leaflet/dist/leaflet.css";
 import "./Map.styles.css";
 
 export default function MapView() {
-  const map = useMap();
+  const [waypoints, setWaypoints] = useState<LatLngExpression[]>([
+    [51.505, -0.09],
+    [51.51, -0.1],
+  ]);
 
-  const waypoints = [
-    [51.505, -0.09], // Start coordinates
-    [51.51, -0.1], // End coordinates
-  ];
+  const map = useMap();
+  console.log(waypoints);
 
   console.log(map);
 
   useEffect(() => {
     if (waypoints && waypoints.length >= 2) {
       L.Routing.control({
-        waypoints: waypoints.map((coord) => L.latLng(coord[0], coord[1])),
+        waypoints: waypoints.map((coord: LatLngExpression) =>
+          L.latLng(coord[0], coord[1])
+        ),
       }).addTo(map);
     }
   }, [map, waypoints]);
+
+  useEffect(() => {
+    map.on("click", function (e) {
+      const coord = e.latlng;
+      const lat = coord.lat;
+      const lng = coord.lng;
+      console.log(
+        "You clicked the map at latitude: " + lat + " and longitude: " + lng
+      );
+
+      setWaypoints((waypoints) => [...waypoints, [lat, lng]]);
+    });
+  }, []);
 
   return (
     <>
